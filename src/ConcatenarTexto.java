7d2ffcddda1b1ddbbe0c60fac505d7c4d9493ec9
GcSanti <kbbl_rsm@hotmail.com>
2022-11-29 18:04:01 -0300

    Primera concatenacion

---
+++ src/ConcatenarTexto.java
@@ -0,0 +1,11 @@
+public class ConcatenarTexto {
+    public static void main(String[] args){
+
+
+        String[] definicion={"Texto:" + " composición de signos " + "codificados en un sistema de escritura"};
+        for (String s : definicion) {
+            System.out.println(s);
+
+        }
+    }
+}
